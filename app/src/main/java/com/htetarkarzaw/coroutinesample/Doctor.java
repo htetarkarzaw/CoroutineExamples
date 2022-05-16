024c6dacef71a64b1790c78a37497b05d7b91132
htetarkarzaw <arkarzaw412@gmail.com>
2022-05-16 17:31:03 +0630

    algorithm for queuing

---
+++ app/src/main/java/com/htetarkarzaw/coroutinesample/Doctor.java
@@ -0,0 +1,10 @@
+package com.htetarkarzaw.coroutinesample;
+
+public class Doctor {
+    String docName = "";
+    int waitingTime = 0;
+    public Doctor(String name,int time){
+        docName = name;
+        waitingTime = time;
+    }
+}
--- app/src/main/java/com/htetarkarzaw/coroutinesample/MainActivity.kt
+++ app/src/main/java/com/htetarkarzaw/coroutinesample/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.htetarkarzaw.coroutinesample
 
-import android.nfc.Tag
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.htetarkarzaw.coroutinesample.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var _binding : ActivityMainBinding? = null
+    private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
     private val TAG = "AppDebug"
     var count = 0
@@ -28,40 +24,81 @@ class MainActivity : AppCompatActivity() {
         main()
         binding.btnHitMe.setOnClickListener {
 //            binding.tvText.text = (count++).toString()
-            solution("abbaacc")
+//            solution("abbaacc")
+            val doctorList = ArrayList<Doctor>()
+            val doc1 = Doctor("Smith", 4)
+            val doc2 = Doctor("Waee", 3)
+            val doc3 = Doctor("Bellie", 2)
+            doctorList.add(doc1)
+            doctorList.add(doc2)
+            doctorList.add(doc3)
+            calculateTime(doctorList, 12)
         }
 
         binding.btnHitMe.setOnLongClickListener {
             binding.tvText.text = ""
             return@setOnLongClickListener true
         }
+
     }
+
+    private fun calculateTime(doctorList: java.util.ArrayList<Doctor>, position: Int): Int {
+        var times = 0
+        var total = 0
+        var answer = 0
+        var count: Int = position
+        for (i in doctorList) {
+            total += i.waitingTime
+        }
+        times = total / doctorList.size * position / doctorList.size
+        for (i in 0 until times) {
+            for (d in doctorList) {
+                if (i % d.waitingTime == 0) {
+                    count -= 1
+                    if (count < 1) {
+                        break
+                    }
+                    answer = i
+                    println("doctor time ->" + i + "---" + d.docName + "--" + answer)
+
+                }
+                if (count < 1) {
+                    break
+                }
+            }
+        }
+
+        println("doctor time for calculate ->$times")
+        println("doctor time ->$answer")
+        return 0
+    }
+
 
     private fun solution(s: String) {
         var list = s.trim().toList()
         var charList = ArrayList<Char>()
         var countList = ArrayList<Int>()
-        for(i in list){
+        for (i in list) {
             var count = 0
             var isAlreadyIn = false
-            for(char in charList) {
-                if (char == i){
-                    isAlreadyIn=true
+            for (char in charList) {
+                if (char == i) {
+                    isAlreadyIn = true
                 }
             }
-            if(!isAlreadyIn) {
+            if (!isAlreadyIn) {
                 charList.add(i)
-                for(j in list){
-                    if(i == j){
-                        count +=1
+                for (j in list) {
+                    if (i == j) {
+                        count += 1
                     }
                 }
                 countList.add(count)
             }
         }
         var finalString = ""
-        for(i in 0 until charList.size){
-            finalString+=countList[i].toString()+charList[i].toString()
+        for (i in 0 until charList.size) {
+            finalString += countList[i].toString() + charList[i].toString()
         }
         println("Test----$finalString")
     }
@@ -78,76 +115,77 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun doNetworkRequest(){
+    private suspend fun doNetworkRequest() {
         println("Starting network request.....")
         delay(5000)
         println("finished network request!")
     }
 
-    private fun printLn(text:String){
-        Log.e(TAG,text)
+    private fun printLn(text: String) {
+        Log.e(TAG, text)
     }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
 
-    fun timeDif(time: String):String{
+    fun timeDif(time: String): String {
         var time1 = time.split("-")[0]
         var time2 = time.split("-")[1]
         var hour1 = time1.split(":")[0].toInt()
         var hour2 = time1.split(":")[0].toInt()
         var min1_zone = time1.split(":")[1]
         var min2_zone = time2.split(":")[1]
-        var min1 = min1_zone.subSequence(0,2).toString().toInt()
-        var min2 = min2_zone.subSequence(0,2).toString().toInt()
-        var zone1 = min1_zone.subSequence(2,4)
-        var zone2 = min2_zone.subSequence(2,4)
-        var formatHour1 =0
-        var formatHour2 =0
+        var min1 = min1_zone.subSequence(0, 2).toString().toInt()
+        var min2 = min2_zone.subSequence(0, 2).toString().toInt()
+        var zone1 = min1_zone.subSequence(2, 4)
+        var zone2 = min2_zone.subSequence(2, 4)
+        var formatHour1 = 0
+        var formatHour2 = 0
         formatHour1 = if (zone1 != "am") {
-            if(hour1==12){
+            if (hour1 == 12) {
                 hour1
-            }else{
-                hour1+12
+            } else {
+                hour1 + 12
             }
         } else {
-            if(hour1==12){
+            if (hour1 == 12) {
                 0
-            }else{
+            } else {
                 hour1
             }
         }
         formatHour2 = if (zone2 != "am") {
-            if(hour1==12){
+            if (hour1 == 12) {
                 hour1
-            }else{
-                hour1+12
+            } else {
+                hour1 + 12
             }
         } else {
-            if(hour2==12){
+            if (hour2 == 12) {
                 0
-            }else{
+            } else {
                 hour2
             }
         }
-        var hourDif = formatHour1-formatHour2
+        var hourDif = formatHour1 - formatHour2
         var minDif = 0
-        if(hourDif<0){
+        if (hourDif < 0) {
             hourDif *= (-1)
-            if(min2<min1){
-                min2+60
+            if (min2 < min1) {
+                min2 + 60
                 hourDif -= 1
             }
-            minDif = min2-min1
-        }else{
-            if(min1<min2){
-                min1+60
+            minDif = min2 - min1
+        } else {
+            if (min1 < min2) {
+                min1 + 60
                 hourDif -= 1
             }
-            minDif = min1-min2
+            minDif = min1 - min2
         }
-        var dif = (hourDif*60)+minDif
+        var dif = (hourDif * 60) + minDif
         printLn("Time-------$formatHour1&$formatHour2---$dif")
         return "$dif"
     }
@@ -158,7 +196,7 @@ class MainActivity : AppCompatActivity() {
             val sdf = SimpleDateFormat("h:mma")
             val date = sdf.parse(myDate)
             date.time
-        }catch (e:Exception){
+        } catch (e: Exception) {
             0
         }
     }
